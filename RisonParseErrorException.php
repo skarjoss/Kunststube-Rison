c1386ed71f31e8500d5beeec4bb62bb07e3949c2
skarjoss <31578794+skarjoss@users.noreply.github.com>
2025-06-15 21:58:29 -0500

    Updated $previous as explicit nullable type to avoid deprecation

--- RisonParseErrorException.php
+++ RisonParseErrorException.php
@@ -2,12 +2,13 @@
 
 namespace Kunststube\Rison;
 
+use Exception;
 
 class RisonParseErrorException extends \RuntimeException {
 
     protected $rison;
 
-    public function __construct($rison, $message, $code = 0, \Exception $previous = null) {
+    public function __construct($rison, $message, $code = 0, ?Exception $previous = null) {
         $this->rison = $rison;
         parent::__construct($message, $code, $previous);
     }
